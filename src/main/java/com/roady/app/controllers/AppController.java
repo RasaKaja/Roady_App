ba3f359043ba849402a6bf893ff75b83249b4977
Your Name <jelena.mikelsone@gmail.com>
2022-08-31 15:42:46 +0300

    index.html added

--- src/main/java/com/roady/app/controllers/AppController.java
+++ src/main/java/com/roady/app/controllers/AppController.java
@@ -1,8 +1,16 @@
 package com.roady.app.controllers;
 
+import com.roady.app.entities.Car;
+import com.roady.app.entities.CustomUserDetails;
+import com.roady.app.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -18,5 +26,67 @@ public class AppController {
     public String viewHomePage() {
         return "index";
     }
+
+// USERS
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        userController.showRegistrationForm(model);
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user){
+        userController.saveUser(user);
+        return "register_success";
+    }
+
+    @GetMapping("/users")
+    public String viewUsersList(Model model){
+        userController.viewUsersList(model);
+        return "users";
+    }
+
+    @GetMapping("/profile")
+    public String viewUserProfile(Model model){
+        userController.viewUsersList(model);
+        return "user_profile";
+    }
+
+    @GetMapping("/profile/edit")  //("/users/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model){
+        userController.showUserById(id, model);
+        return "user_form";
+    }
+
+//    @GetMapping("/my_info")  //("/users/edit/{id}")
+//    public String userInfo(@PathVariable("id") Long id, Model model){
+//        userController.showUserById(id, model);
+//        return "my_info";
+//    }
+
+    @GetMapping("/my_info")
+    public String activeUserInfo(@AuthenticationPrincipal CustomUserDetails user, Model model){
+        model.addAttribute("user", user);
+        return "my_info";
+    }
+
+//    @GetMapping("/profile/edit{id}")  //("/users/edit/{id}")
+//    public String editUser(@PathVariable("id") Long id, Model model){
+//        userController.showUserById(id, model);
+//        return "user_profile";
+//    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        userController.saveUser(user);
+        return "redirect:/profile";
+    }
+
+// CARS
+//    @GetMapping
+//    public String viewAllCarsList(Model model){
+//        carController.viewAllCarsList(model);
+//        return "allCars";
+//    }
 
 }
