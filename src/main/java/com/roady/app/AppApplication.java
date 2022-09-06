b3cd19a88637ccdf7ab985f3ff9b64677a473637
Your Name <jelena.mikelsone@gmail.com>
2022-09-06 16:08:48 +0300

    few html pages are formatted, but improvements are still needed. In formatted pages links are working. Localhost does not load styling sheet.

--- src/main/java/com/roady/app/AppApplication.java
+++ src/main/java/com/roady/app/AppApplication.java
@@ -1,13 +1,19 @@
 package com.roady.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 
 @SpringBootApplication
+
 public class AppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AppApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/roady/app/controllers/AppController.java
+++ src/main/java/com/roady/app/controllers/AppController.java
@@ -1,10 +1,14 @@
 package com.roady.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+
 public class AppController {
 
     @Autowired
