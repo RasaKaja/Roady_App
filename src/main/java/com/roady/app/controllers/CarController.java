748f0a65b601eaa851cd9bf12e2558c3028740e4
Your Name <jelena.mikelsone@gmail.com>
2022-09-11 21:38:27 +0300

    car can be added to database

--- src/main/java/com/roady/app/controllers/CarController.java
+++ src/main/java/com/roady/app/controllers/CarController.java
@@ -2,10 +2,13 @@ package com.roady.app.controllers;
 
 import com.roady.app.entities.Car;
 import com.roady.app.services.CarService;
+import com.roady.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,6 +18,8 @@ public class CarController {
     @Autowired
     private CarService carService;
 
+    @Autowired
+    private UserController userController;
 //    @GetMapping
 //    public String viewAllCarsList(Model model){
 //        List<Car> carList = carService.allCarsList();
@@ -23,8 +28,22 @@ public class CarController {
 //    }
 
     @GetMapping("/cars")
-    public String showYourCarsPage(){
+    public String showYourCarsPage(
+            Model model
+    ){
+//        userController.currentUser.getId();
+
         return "cars";
+    }
+
+    @PostMapping("/cars")
+    public String addCar(String carType, String plateNumber){
+        Car car = new Car();
+        car.setCarType(carType);
+        car.setPlateNumber(plateNumber);
+        car.setUser(userController.currentUser);
+        carService.saveNewCar(car);
+        return "redirect:driver_today";
     }
 
     @GetMapping("/driver_today")
--- src/main/java/com/roady/app/repositories/CarRepository.java
+++ src/main/java/com/roady/app/repositories/CarRepository.java
@@ -1,8 +1,13 @@
 package com.roady.app.repositories;
 
 import com.roady.app.entities.Car;
+import com.roady.app.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
 
+    ArrayList<Car> findAllByUser(User user);
 }
