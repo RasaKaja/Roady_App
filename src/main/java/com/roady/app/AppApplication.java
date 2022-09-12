072b0c323f344e7e9dbbda3bfaddd7549151a5fb
Your Name <jelena.mikelsone@gmail.com>
2022-09-12 20:08:57 +0300

    cars can be added and displayed + ride from master branch

--- src/main/java/com/roady/app/AppApplication.java
+++ src/main/java/com/roady/app/AppApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class AppApplication {
 	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
 
 	}
 }
